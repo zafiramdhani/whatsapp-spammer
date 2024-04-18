cd18b89e69738fa08aa1718c774c98648f6da9cb
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2024-04-18 12:22:43 +0700

    Add paste method

--- spamwa.py
+++ spamwa.py
@@ -7,10 +7,20 @@ print("Running...")
 i = 0
 time.sleep(5)
 
+# ============!!USE ONLY ONE METHOD AT A TIME!!==============
+
+# TYPING METHOD (slower for long messages & limited chars)
+# while i < int(limit):
+#   pt.typewrite(message)
+#   pt.press("enter")
+#   time.sleep(0.100)
+#   i += 1
+
+# PASTE METHOD (faster for long messages, input message value will be ignored)
 while i < int(limit):
-  pt.typewrite(message)
+  pt.hotkey('ctrl', 'v')
   pt.press("enter")
-  time.sleep(0.200)
+  time.sleep(0.100)
   i += 1
 
 print("Stopped.")
